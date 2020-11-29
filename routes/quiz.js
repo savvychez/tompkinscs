8409f2d44892e49e4c7dbc610f24c39fd3e81c84
unknown <savvychez@gmail.com>
2020-11-29 17:00:52 -0600

    added values

--- routes/quiz.js
+++ routes/quiz.js
@@ -14,7 +14,7 @@ module.exports = (sequelize) => {
         const uid = req.body.id
 
         axios.post("https://maker.ifttt.com/trigger/new_quiz/with/key/b29qT57zYRHzFGTGlOqgq7", {
-            "Value1": name, "Value2": diff, "Value3": id
+            "value1": name, "value2": diff, "value3": id
         })
 
         const Tests = require('../models/tests')(sequelize)
